84ff176ab37db7a506d326a2c34ab3be810b7f37
Stefan Stoichev <stefan.stoichev@gmail.com>
2023-05-20 07:23:53 +0100

    Auto stash before merge of "main" and "origin/main"

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,29 @@ import replace from "@rollup/plugin-replace";
 import { readFileSync } from "fs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+// import polyfill from "rollup-plugin-polyfill-node";
 
 const pkg = JSON.parse(readFileSync("./package.json"));
 
 export default [
   {
     input: "src/index.ts",
-    output: [
-      {
-        sourcemap: true,
-        file: pkg.main,
-        format: "es",
-      },
-    ],
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-      "fs",
-      "https",
-      "os",
-    ],
+    output: {
+      sourcemap: true,
+      // file: pkg.main,
+      dir: "dist",
+      format: "es",
+    },
+
+    external: ["fs", "https", "os"],
     plugins: [
       del({
         targets: "dist/*",
       }),
+      // polyfill(),
+      resolve(),
+      commonjs(),
       typescript(),
       replace({
         values: {
@@ -39,13 +38,12 @@ export default [
   },
   {
     input: "check-version.js",
-    output: [
-      {
-        sourcemap: false,
-        file: "dist/check-version.js",
-        format: "es",
-      },
-    ],
+    output: {
+      sourcemap: false,
+      file: "dist/check-version.js",
+      format: "es",
+    },
+
     external: ["fs"],
     plugins: [commonjs(), nodeResolve()],
   },
